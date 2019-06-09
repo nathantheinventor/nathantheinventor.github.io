8502e8a4496ecf4398a3b42e24f734bb9d844808
Nathan Collins <nathantheinventor@gmail.com>
2019-06-09 15:12:53 -0400

    Update

--- static/scripts/general.js
+++ static/scripts/general.js
@@ -4,3 +4,23 @@ function openIfNotLink(event, url) {
         window.open(url);
     }
 }
+
+// function fixCards() {
+//     var w = $(window).width();
+//     if (w >= 768 && w < 992) {
+//         $(".col")
+//     } else if (w >= 992) {
+//         $(".col-md-4").each(function() {
+//             var i = $(this).index();
+//             if (i >= 3) {
+//                 var above = $(".col-md-4:eq(" + (i - 3) + ")");
+//                 var aboveBottom = above.offset().top + above.height();
+//                 var thisTop = $(this).offset().top;
+//                 $(this).css("margin-top", aboveBottom - thisTop);
+//             }
+//         });
+//     }
+// }
+
+// $(window).resize(fixCards);
+// $(document).ready(fixCards);
